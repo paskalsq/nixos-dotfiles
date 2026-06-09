31208b6e84db356a58ccc5dcba9f5bab59e75e22
paskalsq <gith@guardgrid.cc>
2026-06-09 17:46:28 +0300

    comments + organized

--- configuration.nix
+++ configuration.nix
@@ -1,25 +1,41 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ 
-      ./modules/thunar.nix
-    ];
+  imports = [
+    ./modules/thunar.nix
+  ];
+
+  system.stateVersion = "26.05";
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+  time.timeZone = "Europe/Moscow";
+
+  # === Boot and Kernel ===
   boot.loader.timeout = 1;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   #boot.kernelModules = [ "openrazer-driver" ];
   boot.kernelParams = [ "8250.nr_uarts=0" ];
-  
-  
 
+  # === Network and Firewall ===
   networking = {
-  networkmanager.enable = true;
-  nameservers = [ "127.0.0.1" "::1" ];
-  hostName = "desktop";
-  networkmanager.dns = "none";
+    networkmanager.enable = true;
+    nameservers = [ "127.0.0.1" "::1" ];
+    hostName = "desktop";
+    networkmanager.dns = "none";
   };
 
+  systemd.services.NetworkManager-wait-online.enable = false;
+
+  networking.nftables.enable = true;
+  networking.firewall = {
+    enable = true;
+    backend = "nftables";
+    allowedTCPPorts = [ 8000 4533 9180 8384 ];
+    allowedUDPPorts = [ ];
+  };
+
+  # === dnscrypt-proxy2 ===
   services.dnscrypt-proxy2 = {
     enable = true;
     settings = {
@@ -36,29 +52,28 @@
         cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
         minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
       };
-
       server_names = [ "quad9-dnscrypt-ip4-filter-pri" "anon-scaleway-fr" ];
     };
   };
 
-  systemd.services.NetworkManager-wait-online.enable = false;
-
-  time.timeZone = "Europe/Moscow";
+  # === GUI, DM and X11 === 
   services.displayManager.ly.enable = true;
+  
   services.xserver = {
     enable = true;
-    displayManager.setupCommands = ''
-    '';
+   # displayManager.setupCommands = '';
     autoRepeatDelay = 200;
     autoRepeatInterval = 35;
     windowManager.qtile.enable = true;
   };
+
   services.xserver.xkb = {
     layout = "us,ru";
     variant = "";
     options = "grp:alt_shift_toggle";
-    };
+  };
 
+  # === (Pipewire) and Security ===
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -67,18 +82,21 @@
     alsa.support32Bit = true;
     jack.enable = true;
   };
-  
-  security.doas = {
-  enable = true;
-  extraRules = [
-    {
-      groups = [ "wheel" ];
-      keepEnv = true;
-      persist = true;
-    }
-  ];
-};
 
+  security.doas = {
+    enable = true;
+    extraRules = [
+      {
+        groups = [ "wheel" ];
+        keepEnv = true;
+        persist = true;
+      }
+    ];
+  };
+  security.sudo.enable = false; 
+  security.pam.services.i3lock.enable = true;
+
+  # === Users and Fonts ===
   fonts.packages = with pkgs; [
     nerd-fonts.jetbrains-mono
   ];
@@ -92,41 +110,53 @@
     ];
   };
 
-  programs.git = {
-    enable = true;
-    config = {
-    safe = {
-      directory = "/home/paskalsq/nixos-dotfiles";
-      };
-    };
-  };
+  # === System and Other Services ===
   services.dbus.enable = true;
   hardware.openrazer.enable = true;
+
   services.syncthing = {
-  enable = true;
-  user = "paskalsq";
-  dataDir = "/home/paskalsq/.config/syncthing";
-  configDir = "/home/paskalsq/.config/syncthing";
-  openDefaultPorts = true;
+    enable = true;
+    user = "paskalsq";
+    dataDir = "/home/paskalsq/.config/syncthing";
+    configDir = "/home/paskalsq/.config/syncthing";
+    openDefaultPorts = true;
   };
 
+  # === Virtualisation and Docker ===
   virtualisation.docker = {
-  enable = true;
-  enableNvidia = true;
-  extraOptions = "--dns 9.9.9.9 --dns 1.1.1.1";
-}; 
+    enable = true;
+    enableNvidia = true;
+    extraOptions = "--dns 9.9.9.9 --dns 1.1.1.1";
+  };
+
+  virtualisation.docker.daemon.settings = {
+    data-root = "/home/paskalsq/docker";
+  };
 
   systemd.services.docker.after = [ "network.target" ];
   systemd.services.docker.requires = [ "network.target" ];
 
-  virtualisation.docker.daemon.settings = {
-  data-root = "/home/paskalsq/docker";
-};
-  
-  nixpkgs.config.allowUnfree = true;
+  # === Programs and Environment Packages ===
+  programs.zsh.enable = true;
+  programs.dconf.enable = true;
+
+  programs.git = {
+    enable = true;
+    config = {
+      safe = {
+        directory = "/home/paskalsq/nixos-dotfiles";
+      };
+    };
+  };
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+
   environment.systemPackages = with pkgs; [
-    vim 
-    git
+    vim
     wget
     alacritty
     pavucontrol
@@ -137,29 +167,4 @@
     android-tools
     pulseaudio
   ];
-  security.pam.services.i3lock.enable = true;
- 
-  programs.zsh.enable = true;
-  programs.dconf.enable = true;
-
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-  };
-
-networking.nftables.enable = true;
-
-networking.firewall = {
-  enable = true;
-  backend = "nftables"; 
-
-  allowedTCPPorts = [ 8000 4533 9180 8384 ];
-  allowedUDPPorts = [ ];
-};
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  system.stateVersion = "26.05"; 
-
 }
-
